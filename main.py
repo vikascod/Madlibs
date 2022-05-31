59e2b217f1c70d5cacf186eabf42c7d8dfb94cce
Vikas <vikas1618072@gmail.com>
2022-05-31 15:06:59 +0800

    madlibs

---
+++ main.py
@@ -0,0 +1,25 @@
+adj1 = 'Programmer'
+adj2 = '01 march,2022'
+name = 'Python'
+adj3 = 'Simple'
+adj4 = 'Easy'
+adj5 = 'Python'
+adj6 = 'Machine Learning'
+adj7 = 'Web Devoloper'
+
+content = '''I am a {} and i started programming on {}, i picked my first programming
+languege as {} after learning this languege i know that how is this languege it\'s really
+{} and {} to learn if you are a beginner the i suggest you choose your first languege 
+{} you can do many thing after learning python like :- {}, {} and etc.'''
+
+print(content.format('01 march,2022', 'Programmer', 'Python', 'Simple', 'Easy', 'Python', 'Machine Learning', 'Web Devoloper'))
+
+
+
+
+# content = f'''I am a {adj1} and i started programming on {adj2}, i picked my first programming
+# languege as {name} after learning this languege i know that how is this languege it\'s really
+# {adj3} and {adj4} to learn if you are a beginner the i suggest you choose your first languege 
+# {adj5} you can do many thing after learning python like :- {adj6}, {adj7} and etc.'''
+
+# print(content)
